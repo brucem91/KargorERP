5f36ca34f5cdd18191aa65392dd9ecdaaea9141c
Bruce Murray <brucem91@gmail.com>
2018-10-08 18:26:10 -0700

    continued work

---
+++ src/client/src/components/tables/BasicTable.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react';
+
+class BasicTable extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            pageSize: Number(props.pageSize || 25),
+            searchable: props.searchable || true,
+            ajaxUrl: props.ajaxUrl,
+            columns: [],
+            records: [],
+            filteredRecords: [],
+            currentPage: 0,
+            maxResponseRecordLimit: 5000
+        };
+    }
+}
+
+export default BasicTable;
