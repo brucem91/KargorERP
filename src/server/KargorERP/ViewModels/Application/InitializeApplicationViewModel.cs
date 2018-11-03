e4a709dc74d8b2d0c781e74cdd9e5f0cdc2232c9
Bruce Murray <brucem91@gmail.com>
2018-11-03 14:05:16 -0700

    continued work

--- src/server/KargorERP/ViewModels/Application/InitializeApplicationViewModel.cs
+++ src/server/KargorERP/ViewModels/Application/InitializeApplicationViewModel.cs
@@ -8,8 +8,18 @@ namespace KargorERP.ViewModels.Application
         [Required]
         public string Name { get; set; }
         [Required]
+        [EmailAddress]
+        [MaxLength(256)]
+        [MinLength(3)]
         public string EmailAddress { get; set; }
+        [Required]
+        [Phone]
+        [MaxLength(256)]
+        [MinLength(3)]
         public string PhoneNumber { get; set; }
+        [Required]
+        [MinLength(8)]
+        [MaxLength(256)]
         public string Password { get; set; }
     }
 }
---
+++ src/test/config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    'url': process.env.URL || 'http://localhost:5000',
+    'username': process.env.USERNAME || 'admin',
+    'password': process.env.PASSWORD || '',
+    'phoneNumber': process.env.PHONE || '(555) 555-1234',
+    'emailAddress': process.env.EMAIL || 'admin@example.com'
+};
